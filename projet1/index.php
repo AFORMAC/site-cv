2ae576eeb526047d2ea8ec2ba9b4a3bd4cd26732
thomas <brownaformac@gmail.com>
2018-01-23 11:18:58 +0100

    push initial

--- projet1/index.php
+++ projet1/index.php
@@ -1,9 +1,55 @@
-<?php
-
-
-  echo "coucou";
-
-
-
-
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+    <title>EXO php</title>
+  <body>
+    <form action="verification.php" method="post">
+      <meaccesskey=""ta charset="utf-8">
+      <fieldset>
+        <legend>Formulaire</legend>
+          <p>
+            <label for="mail">E-mail :</label><br>
+            <input type="text" name="mail" value="">
+          </p>
+          <p>
+            <label for="prenom">Prénom :</label><br>
+            <input type="text" name="prenom" value="">
+          </p>
+          <p>
+            <label for="nom">Nom :</label><br>
+            <input type="text" name="nom" value="">
+          </p>
+          <p>
+            <label for="adresse">Adresse :</label><br>
+            <input type="text" name="adresse" value="">
+          </p>
+          <p>
+            <label for="age">Âge :</label><br>
+            <input type="text" name="age" value="">
+          </p>
+          <p>
+            <label for="num">Numéro :</label><br>
+            <input type="text" name="num" value="" id="envoi">
+          </p>
+          <p>
+            <input type="checkbox" name="news" value=""> m'inscrire à la newsletter
+          </p>
+          <p>
+            <input type="submit" name="envoi" value="ENVOYER">
+          </p>
+          <div>
+            <?php
+              if(isset($_GET['toto'])){
+            ?>
+            <p>
+              <?php echo $_GET['toto']; ?>
+            </p>
+           <?php
+            }
+           ?>
+         </div>
+      </fieldset>
+    </form>
+  </body>
+</html>
---
+++ projet1/verification.php
@@ -0,0 +1,25 @@
+<?php
+
+  $error = "Penser à remplir : ";
+  $ok = "ok";
+  $champ = array(
+    'mail' =>  $_POST['mail'],
+    'prenom' => $_POST['prenom'],
+    'nom' => $_POST['nom'],
+    'adresse' => $_POST['adresse'],
+    'age' => $_POST['age'],
+    'num' => $_POST['num']
+  );
+
+  foreach ($champ as $key => $value)
+  {
+    if ($value == "") {
+        $error = $error . " " . $key;
+        header('Location:index.php?toto='.$error);
+
+    } else {
+        header('Location:index.php?toto='.$ok);
+    }
+  }
+
+?>
